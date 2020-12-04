ba5f93f799f79e1ccfcec940201155b2403cd88f
Luke Hammer <textchimp@gmail.com>
2020-12-04 17:25:49 +1100

    Mixtapes crud with authorisation/permissions checking

--- week5/tunr-db/app/controllers/mixtapes_controller.rb
+++ week5/tunr-db/app/controllers/mixtapes_controller.rb
@@ -12,7 +12,26 @@ class MixtapesController < ApplicationController
     # So we need to lock down the mixtape routes, to ensure that
     # only logged-in users can see them
 
-    mixtape = Mixtape.create mixtape_params
+    # Mixtape.new with strong params creates the object in memory, with
+    # the column attributes filled in from the form params... but not saved to DB
+    mixtape = Mixtape.new mixtape_params
+    mixtape.user_id = @current_user.id # this mixtape belongs to the logged-in user
+    mixtape.save  # actually do DB 'INSERT'
+
+    # Add the user ID to this mixtape
+
+    # 1. Set attribute after .create, and then .save
+    # mixtape.user_id = @current_user.id
+    # mixtape.save
+
+    # 2. Use .update instead of setting attribute and .save as separate steps
+    # mixtape.update user_id: @current_user.id
+
+    # 3. Use the '<<' to add to a many association
+    # @current_user.mixtapes << mixtape
+
+
+
     redirect_to mixtape_path( mixtape.id ) # go to show page
   end
 
@@ -24,10 +43,24 @@ class MixtapesController < ApplicationController
   end
 
   def edit
+    @mixtape = Mixtape.find params[:id]
+    redirect_to login_path unless @mixtape.user_id == @current_user.id
   end
 
   def update
-  end
+    mixtape = Mixtape.find params[:id]
+
+    # Don't perform the edit unless the mixtape belongs to the user
+    # redirect_to login_path and return unless mixtape.user_id == @current_user.id
+    if mixtape.user_id != @current_user.id
+      redirect_to login_path
+      return
+    end #if
+
+    mixtape.update mixtape_params # strong params
+    redirect_to mixtape_path(mixtape.id)
+  end #update
+
 
   def destroy
   end
