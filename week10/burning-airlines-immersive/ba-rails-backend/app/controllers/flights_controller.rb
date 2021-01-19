39b0b03589ebb1b5949a8b7b6a8ad993cb3c3295
Luke Hammer <textchimp@gmail.com>
2021-01-19 19:08:35 +1100

    Rails backend additions for Vue frontend, including associations with JSON render etc

--- week10/burning-airlines-immersive/ba-rails-backend/app/controllers/flights_controller.rb
+++ week10/burning-airlines-immersive/ba-rails-backend/app/controllers/flights_controller.rb
@@ -1,7 +1,14 @@
 class FlightsController < ApplicationController
 
   def search
-    render json: Flight.where( origin: params[:origin], destination: params[:destination] )
+    flights = Flight.where( origin: params[:origin], destination: params[:destination] )
+    render json: flights,
+      except: [:airplane_id, :created_at, :updated_at], # leave out of flight info
+      methods: [:departure_date_formatted], # include the departure_date_formatted method
+      include: {
+        airplane: { only: [:name] } # only include the airplane association's name field
+      }
+
   end # search
 
   def show
--- week10/burning-airlines-immersive/ba-rails-backend/app/models/flight.rb
+++ week10/burning-airlines-immersive/ba-rails-backend/app/models/flight.rb
@@ -3,4 +3,8 @@ class Flight < ApplicationRecord
   belongs_to :airplane
   has_many :reservations
 
+  def departure_date_formatted
+    self.departure_date.strftime('%d %b %Y %I:%M%P')
+  end
+
 end
